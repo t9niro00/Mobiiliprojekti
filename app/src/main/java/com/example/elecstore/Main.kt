041bb3203788b7d5f624f6c0d3fa1db7ea6f1e5b
EinoAnttila <e-ino@hotmail.com>
2021-03-17 16:45:07 +0200

    testi commit Eino

--- app/src/main/java/com/example/elecstore/Main.kt
+++ app/src/main/java/com/example/elecstore/Main.kt
@@ -2,5 +2,5 @@ package com.example.elecstore
 
 
 fun main(){
-    println("Moi kamerutset! Kokeilkaas pullata tätä!")
+    println("Moi kamerutset! Kokeilkaas pullata tätä! asdasdasdasdasdassdasdasd")
 }
