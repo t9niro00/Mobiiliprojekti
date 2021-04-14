06b1ba32511876f95891d0633830832c884e2d49
Petteri Longi <79117257+PetteriLongi@users.noreply.github.com>
2021-04-14 12:02:47 +0300

    UI etenee

--- app/src/main/java/com/example/komponenttikirjasto/MainActivity.kt
+++ app/src/main/java/com/example/komponenttikirjasto/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.komponenttikirjasto
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.komponenttikirjasto.R.id.buttonMikropiirit
+import com.example.komponenttikirjasto.R.id.buttonKomponentit
+import com.example.komponenttikirjasto.R.id.textViewTitle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(buttonMikropiirit).setOnClickListener {
+            val intent = Intent(this, mikrokontrollerit_selaus::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(buttonKomponentit).setOnClickListener {
+            val intent = Intent(this, komponentit_selaus::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(textViewTitle).setOnClickListener {
+            val intent = Intent(this, settings::class.java)
+            startActivity(intent)
+        }
+
+        checkTheme()
+    }
+// nappulat joilla siirtyä toisiin aktiviteetteihin
+
+    private fun checkTheme() {
+        when (MyPreferences(this).darkMode) {
+            0 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                delegate.applyDayNight()
+            }
+            1 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                delegate.applyDayNight()
+            }
+            2 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                delegate.applyDayNight()
+            }
+        }
     }
 }
+//kun app aukeaa tämä tarkistaa mikä teema viimeksi asetettuna ja automaattisesti ottaa tämän käyttöön
+
+class MyPreferences(context: Context?) {
+
+    companion object {
+        private const val DARK_STATUS = "io.github.manuelernesto.DARK_STATUS"
+    }
+
+    private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    var darkMode = preferences.getInt(DARK_STATUS, 0)
+        set(value) = preferences.edit().putInt(DARK_STATUS, value).apply()
+}
+//teema haku
