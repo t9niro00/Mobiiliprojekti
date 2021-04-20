dbd2588a5a19eb05ac4dbd3065fce1cceddc26b1
t9niro00 <t9niro00@students.oamk.fi>
2021-04-20 12:20:44 +0300

    Nyt muokattu koodia siten, että luku ja kirjoitus onnistuu kolmella eri sivustolla.

--- app/src/main/java/com/example/komponenttikirjasto/MainActivity.kt
+++ app/src/main/java/com/example/komponenttikirjasto/MainActivity.kt
@@ -1,54 +1,63 @@
 package com.example.komponenttikirjasto
 
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import com.example.elecstore.RealtimeDatabase
+import com.example.elecstore.getData
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.komponentit_selaus.*
+import org.w3c.dom.Text
 import java.lang.StringBuilder
+
+
 
 // Asetetaan data
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.komponentit_selaus)
+        setContentView(R.layout.activity_main)
 
-        var database = FirebaseDatabase.getInstance().getReference("Products")
+        //Asetetaan databasen oksien nimet.
 
         var name1 = "Komponentit"
         var name2 = "Mikrokontrollerit"
 
+        //Määritellään polku, josta lähdetään liikkeelle databasessa
 
-        database.child(name1).setValue(RealtimeDatabase("Infra", 1, 6))
-        database.child(name2).setValue(RealtimeDatabase("Puna", 2, 7))
-        database.addValueEventListener(getData)
-        database.addListenerForSingleValueEvent(getData)
-    }
+        var database = FirebaseDatabase.getInstance().getReference("Products")
 
-    // Haetaan data
+        //Asetetaan databaseen halutut arvot koodissa.
 
-    var getData = object : ValueEventListener{
-        override fun onCancelled(p0: DatabaseError) {
+        database.child(name1).setValue(RealtimeDatabase("OMEGA", 1, 2))
+        database.child(name2).setValue(RealtimeDatabase("LUL", 3, 4))
 
-        }
+        //Asetetaan listeneri mikropiirien valintapainikkeeseen,
+        //jota painaessa suoritetaan getData-luokan sisällä oleva koodi
 
-        override fun onDataChange(p0: DataSnapshot) {
-            var sbname = StringBuilder()
-            var sbprice = StringBuilder()
-            for ( i in p0.children){
-                var prodname1 = i.child("prodname").getValue()
-                var prodprice1 = i.child("prodprice").getValue()
-                sbname.append("$prodname1")
-                sbprice.append("$prodprice1€")
-            }
-            textViewTuotenimiKO1.setText(sbname)
-            textViewHintaKO1.setText(sbprice)
+        buttonMikropiirit.setOnClickListener{
+            startActivity(Intent(applicationContext,getData::class.java))
         }
 
     }
-
 }
+
+
+
+
+
+
+
+
+
